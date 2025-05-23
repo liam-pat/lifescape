d9fa3e7ab552c1b4371cf06f727c7aee8aa623c5
Packie <Packie.yy.bi@viu.com>
2025-05-23 11:01:19 +0800

    feat: fixed rss.xml override issue

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
 
   return rss({
-    title: '我的生活與閱讀分享',
+    title: 'Mr.Pat lifescape',
     description: '分享生活點滴與讀書心得',
     site: context.site,
     items: allPosts.map((post) => ({
@@ -20,6 +20,17 @@ export async function GET(context) {
       link: `/${post.collection}/${post.slug}/`,
       categories: post.data.tags || [],
     })),
-    customData: `<language>zh-CN</language>`,
+    customData: `
+      <language>zh-CN</language>
+      <image>
+        <url>https://s21.ax1x.com/2025/05/23/pEztyvQ.png</url>
+        <title>Mr.Pat Lifescape</title>
+        <link>https://life.biyongyao.com</link>
+      </image>
+      <follow_challenge>
+        <feedId>148564006964797440</feedId>
+        <userId>83722505120690176</userId>
+      </follow_challenge>
+    `,
   });
 } 
